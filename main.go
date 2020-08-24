454c69c90f5a44d3b2bdbb16764a1e9da2bc0752
Tomi Amao <tomi@amao.io>
2020-08-24 20:37:50 +0100

    Create main.go

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"example-golang-postgres-app/router"
+	"log"
+	"net/http"
+)
+
+func main() {
+	r := router.Router()
+	fmt.Sprintf("Starting server on the port %s...\n", os.Getenv("PORT"))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", os.Getenv("PORT")), r))
+}
