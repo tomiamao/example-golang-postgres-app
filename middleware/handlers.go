19c780141a8a92f3bc328936becde72d675f1350
Tomi Amao <tomi@amao.io>
2020-08-25 13:03:10 +0100

    Update handlers.go

--- middleware/handlers.go
+++ middleware/handlers.go
@@ -46,6 +46,10 @@ func createConnection() *sql.DB {
 		panic(err)
 	}
 	
+	if _, err = db.Exec("DROP TABLE users"); err != nil {
+        	log.Fatal(err)
+	}
+	
 	if _, err = db.Exec("CREATE TABLE IF NOT EXISTS users (id SERIAL, name TEXT, location TEXT, age INT)"); err != nil {
         	log.Fatal(err)
 	}
